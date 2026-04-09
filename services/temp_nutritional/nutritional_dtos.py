15de4183f009e3f7f301f266427b24d671757fa6
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2026-04-09 19:14:12 +0000

    feat: add NrsScoreDTO

---
+++ services/temp_nutritional/nutritional_dtos.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
+class NrsScoreDTO:
+    id: int
+    nrs_nut: Optional[int]
+    nrs_doenca: int
+    nrs_idade: int
+    nrs_total: int
+    nrs_completo: bool
+    nrs_ref_at: datetime
+    calculado_at: datetime
