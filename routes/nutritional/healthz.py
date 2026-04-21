c6e72368131ac3baa2927e2c3e564fbdc07748a1
Will0301 <willian.f.w@hotmail.com>
2026-04-21 00:20:10 -0300

    test: fixed the code

--- routes/nutritional/healthz.py
+++ routes/nutritional/healthz.py
@@ -5,4 +5,4 @@ app_nutritional_health = Blueprint("app_nutritional_health", __name__)
 
 @app_nutritional_health.route("/nutritional/healthz", methods=["GET"])
 def healthz():
-    return {"Message": "Ok"
+    return {"Message": "Ok"}
