57e7e1caf1bda41660e915545e82ef7963eecd0d
HenriqueKnack22 <h.knack@edu.pucrs.br>
2026-06-04 17:41:31 -0300

    add: implemetacao hist e cond

--- services/nutritional/nutritional_active_patients_service.py
+++ services/nutritional/nutritional_active_patients_service.py
@@ -100,13 +100,13 @@ def get_patients(request_data: NutritionalPatientsRequest):
                 "glim_fen": glim_fen,
                 "glim_etiol": glim_etiol,
                 "inst": [],  # from demo.nutricional_alerta - empty for now
-                # "conduta": row.conduta,
+                "conduta": row.conduta,
                 "haval": haval,
                 "d7": d7,
                 "pri": idx,  # position in the priority queue
                 "sev": sev,
                 "freq_horas": freq_horas,
-                "hist": [],  # empty in this US
+                "hist": row.hist if row.hist else [],
             }
         )
 
